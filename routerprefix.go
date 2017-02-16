b25d91c94592c950dd60b2630d7227f8f22fef7f
cssivision <cssivision@gmail.com>
2017-02-16 21:47:37 +0800

    feat: remove compose

--- routerprefix.go
+++ routerprefix.go
@@ -81,3 +81,19 @@ func copyHandlers(dst, src []HandlerFunc) {
 		dst[index] = val
 	}
 }
+
+func compose(handlers []HandlerFunc) router.Handle {
+    return func(rw http.ResponseWriter, req *http.Request, ps router.Params) {
+    	context := &Context{
+    		Request: req,
+    		handlers: handlers,
+    		current: -1,
+    		ResponseWriter: ResponseWriter{
+    			ResponseWriter: rw,
+    		},
+    		Params: ps,
+    	}
+
+    	context.Next()
+    }
+}
