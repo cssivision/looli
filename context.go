e62fc1211b2b30b299031810c73fc3e08f89f37b
cssivision <cssivision@gmail.com>
2017-02-20 18:29:53 +0800

    feat: engine add IgnoreCase, TrailingSlashRedirect

--- context.go
+++ context.go
@@ -56,7 +56,11 @@ func (c *Context) Abort() {
 	c.current = abortIndex
 }
 
+// AbortWithStatus prevents pending handlers from being called and set statuscode. Note that this will not 
+// stop the current handler. if you want to stop current handler you should return, after call abort, call 
+// Abort to ensure the remaining handlers for this request are not called.
 func (c *Context) AbortWithStatus(code int) {
+	c.statusCode = code
 	c.Abort()
 }
 
--- looli.go
+++ looli.go
@@ -7,9 +7,25 @@ import (
 
 type (
 	Engine struct {
+		// router with basePath
 		RouterPrefix
-		router              *router.Router
+
+		// router used to match url
+		router *router.Router
+
+		// when set true, implements a best effort algorithm to return the real client IP, it parses
+		// X-Real-IP and X-Forwarded-For in order to work properly with reverse-proxies such us: nginx or haproxy.
 		ForwardedByClientIP bool
+
+		// Enables automatic redirection if the current route can't be matched but a
+		// handler for the path with (without) the trailing slash exists.
+		// TrailingSlashRedirect: /a/b/ -> /a/b
+		// TrailingSlashRedirect: /a/b -> /a/b/
+		// default value is true
+		TrailingSlashRedirect bool
+
+		// Ignore case when matching URL path.
+		IgnoreCase bool
 	}
 	HandlerFunc func(*Context)
 )
@@ -22,15 +38,26 @@ func New() *Engine {
 		router: router.New(),
 	}
 
+	engine.RouterPrefix.router.TrailingSlashRedirect = true
+	engine.RouterPrefix.router.IgnoreCase = false
 	engine.RouterPrefix.router = engine.router
 	engine.RouterPrefix.engine = engine
 	return engine
 }
 
+// Default return engine instance, add logger, recover handler to it.
 func Default() *Engine {
 	engine := New()
 	engine.RouterPrefix.Handlers = []HandlerFunc{Logger(), Recover()}
 	return engine
+}
+
+func (engine *Engine) SetIgnoreCase(flag bool) {
+	engine.RouterPrefix.router.IgnoreCase = flag
+}
+
+func (engine *Engine) SetTrailingSlashRedirect(flag bool) {
+	engine.RouterPrefix.router.TrailingSlashRedirect = flag
 }
 
 func (engine *Engine) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
