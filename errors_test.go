5f0ea534ba36f985f326ef83d6ec65b0db8a60b3
cssivision <cssivision@gmail.com>
2017-03-02 16:20:29 +0800

    feat: add test for Error

--- errors_test.go
+++ errors_test.go
@@ -1,1 +1,29 @@
 package looli
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestNewError(t *testing.T) {
+	data := JSON{
+		"name": "cssivision",
+	}
+
+	err := &Error{
+		Err:  errors.New("error happen"),
+		Code: http.StatusOK,
+		Meta: data,
+	}
+
+	assert.Equal(t, "error happen", err.Error())
+	assert.Equal(t, http.StatusOK, err.Code)
+	want, e := json.Marshal(data)
+	assert.Nil(t, e)
+	get, e := json.Marshal(err.Meta)
+	assert.Nil(t, e)
+	assert.Equal(t, want, get)
+}
