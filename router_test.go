bc66c6e0570260eac69e2e56a83026c50d295c12
cssivision <cssivision@gmail.com>
2017-03-10 20:17:56 +0800

    feat: add test for router

--- router_test.go
+++ router_test.go
@@ -1,9 +1,15 @@
 package looli
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewRouter(t *testing.T) {
-
+	router := NewRouter()
+	assert.True(t, router.TrailingSlashRedirect)
+	assert.NotNil(t, router.allowMethods)
+	assert.NotNil(t, router.tree)
+	assert.NotNil(t, router.tree.children)
+	assert.NotNil(t, router.tree.handlers)
 }
